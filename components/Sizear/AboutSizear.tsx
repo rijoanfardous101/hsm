0d45261e2cf9fbcbcca1c83fea1b178cabd26d9d
Rijoan Fardous <rijoanfardous101@gmail.com>
2025-04-16 22:12:41 +0600

    email icon changed

--- components/Sizear/AboutSizear.tsx
+++ components/Sizear/AboutSizear.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { MdOutlineEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
 
 const AboutSizear = () => {
@@ -12,7 +11,7 @@ const AboutSizear = () => {
 
       <div className="relative max-w-6xl mx-auto flex flex-col md:flex-row gap-10 z-10 justify-between">
         {/* Left content */}
-        <div className="w-full md:w-[80%] text-gray-800">
+        <div className="w-full md:w-[80%] text-gray-800 -mt-6">
           {/* Name */}
           <h1 className="text-[36px] font-bold text-gray-900">
             <span className="text-[#0070c0]">Monaemul</span>{" "}
@@ -39,18 +38,6 @@ const AboutSizear = () => {
             |{" "}
             <span className="text-purple-600 font-medium">Aspiring Writer</span>
           </p>
-
-          <div className="flex gap-3 items-center mt-2">
-            <a
-              href="https://www.linkedin.com/in/monaemulsizear/"
-              target="_blank"
-            >
-              <FaLinkedin className="text-[26px] text-blue-700 hover:text-blue-900" />
-            </a>
-            <a href="mailto:sizear.monaemul@gmail.com" className="">
-              <MdOutlineEmail className="text-[30px]" />
-            </a>
-          </div>
 
           {/* Paragraphs */}
           <div className="mt-6 space-y-5  text-gray-700">
@@ -89,14 +76,106 @@ const AboutSizear = () => {
         </div>
 
         {/* Right image */}
-        <div className="flex justify-center md:justify-end items-start">
-          <Image
-            src="/sizearImage.webp"
-            alt="Monaemul Islam Sizear"
-            width={300}
-            height={200}
-            className="rounded-2xl w-full"
-          />
+        <div className="">
+          <div>
+            <Image
+              src="/sizearImage.webp"
+              alt="Monaemul Islam Sizear"
+              width={300}
+              height={200}
+              className="rounded-2xl w-full"
+            />
+          </div>
+
+          <div className="flex gap-3 items-center mt-4 justify-center">
+            <a
+              href="https://www.linkedin.com/in/monaemulsizear/"
+              target="_blank"
+            >
+              <FaLinkedin className="text-[26px] text-blue-700 hover:text-blue-900" />
+            </a>
+            <a href="mailto:sizear.monaemul@gmail.com" className="">
+              {/* <MdEmail className="text-[30px] text-" /> */}
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="30"
+                height="30"
+                x="0"
+                y="0"
+                viewBox="0 0 512 512"
+                // style={{ enableBackground: "new 0 0 512 512" }}
+                xmlSpace="preserve"
+              >
+                <g>
+                  <rect
+                    width="446"
+                    height="305.613"
+                    fill="#e57e25"
+                    rx="25.31"
+                    opacity="1"
+                    data-original="#e57e25"
+                  ></rect>
+                  <path
+                    fill="#f29c1f"
+                    d="M435.739 300.212A26.13 26.13 0 0 0 446 279.434V26.179A26.179 26.179 0 0 0 419.821 0H166.508L435.2 300.014z"
+                    opacity="1"
+                    data-original="#f29c1f"
+                  ></path>
+                  <path
+                    fill="#f29c1f"
+                    d="M279.492 0H26.179A26.179 26.179 0 0 0 0 26.179v253.255a26.124 26.124 0 0 0 9.7 20.334l1.1.246z"
+                    opacity="1"
+                    data-original="#f29c1f"
+                  ></path>
+                  <path
+                    fill="#f0c419"
+                    d="M419.821 0H26.179A26.179 26.179 0 0 0 0 26.179v10.063l188.916 130.177a60.068 60.068 0 0 0 68.168 0L446 36.242V26.179A26.179 26.179 0 0 0 419.821 0z"
+                    opacity="1"
+                    data-original="#f0c419"
+                  ></path>
+                  <path
+                    fill="#ff5364"
+                    d="M116.126 512a8 8 0 0 1-5.157-14.119l110.588-91.916a8 8 0 1 1 10.306 12.235l-110.587 91.919a7.97 7.97 0 0 1-5.15 1.881z"
+                    opacity="1"
+                    data-original="#ff5364"
+                  ></path>
+                  <path
+                    fill="#35acef"
+                    d="M209.927 494.588a8 8 0 0 1-5.275-14.018l27.222-24.87a8 8 0 1 1 10.543 12.035l-27.217 24.87a7.967 7.967 0 0 1-5.273 1.983z"
+                    opacity="1"
+                    data-original="#35acef"
+                  ></path>
+                  <path
+                    fill="#285680"
+                    d="m239.598 336.348 50.471 120.895 92.207-138.322 129.63-123.515z"
+                    opacity="1"
+                    data-original="#285680"
+                  ></path>
+                  <path
+                    fill="#7ed0fc"
+                    d="m511.906 194.516-266.03 155.177-78.277-20.307z"
+                    opacity="1"
+                    data-original="#7ed0fc"
+                  ></path>
+                  <path
+                    fill="#35acef"
+                    d="M418.218 297.138 290.069 457.243l15.123-91.701z"
+                    opacity="1"
+                    data-original="#35acef"
+                  ></path>
+                  <path
+                    fill="#7ed0fc"
+                    d="M511.906 195.406c.073-.227.169-.4 0-.232L305.192 365.542 433.246 400.4z"
+                    opacity="1"
+                    data-original="#7ed0fc"
+                  ></path>
+                </g>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </section>
